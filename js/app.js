c7f860bd5f73406da29a0df85bfaa067dfa662c3
McMartinezc <psyence_mc@hotmail.com>
2023-10-25 09:34:24 +0200

    finished

--- js/app.js
+++ js/app.js
@@ -3,21 +3,39 @@ const pictures = [
   "contBcg-1",
   "contBcg-2",
   "contBcg-3",
-  "contBcg-4"
+  "contBcg-4",
 ];
 
-// buena suerte
+// Índice de la imagen actual
+let currentImageIndex = 0; 
+
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
 const container = document.querySelector(".img-container");
 
-container.addEventListener("click", (event)=>{
-  console.log(event.currentTarget)
-  if(event.target.classList.contains("btn-left")){
-    console.log("izquierda")
-  }else if(event.target.classList.contains("btn-right")){
-    console.log("derecha")
+btnLeft.addEventListener("click", () => {
+  currentImageIndex--; //Restamos posición al indice
+  console.log(currentImageIndex);
+
+  //Verificamos que el indice es negativo, si lo es igualamos el indece negativo al ultimo elemento del array
+  if (currentImageIndex < 0) {
+    currentImageIndex = pictures.length - 1;
+  }
+  const imgName = pictures[currentImageIndex];
+  const imgUrl = `../img/${imgName}.jpeg`;
+  container.style.backgroundImage = `url(${imgUrl})`;
+});
+
+btnRight.addEventListener("click", () => {
+  currentImageIndex++; // Mueve el índice a la derecha
+  console.log(currentImageIndex);
+  
+  //Si nos salimos del array, tendremos que volver a 0
+  if (currentImageIndex >= pictures.length) {
+    currentImageIndex = 0;
   }
 
-  
-})
+  const imgName = pictures[currentImageIndex];
+  const imgUrl = `../img/${imgName}.jpeg`;
+  container.style.backgroundImage = `url(${imgUrl})`;
+});
